1c02de9ac37abfb73c5c5dbda934552503484710
Maurício Theodoro <138634517+BOT155@users.noreply.github.com>
2023-10-20 19:42:38 -0300

    up

--- Esusten/app.js
+++ Esusten/app.js
@@ -1,1 +1,11 @@
-var btnsignin = document.querySelector("#signin");
+var btnsignin = window.document.getElementById('signin');
+var btnsignup = window.document.getElementById('signup');
+
+var body = window.document.querySelector("body");
+
+btnsignin.addEventListener("click", function () {
+    body.className = "sign-in-js";
+});
+btnsignup.addEventListener("click", function () {
+    body.className = "sign-up-js";
+});
